b4857b385c8556479020a589885aa8626b87f295
erik.7z <nguita.erik@gmail.com>
2022-10-26 13:17:55 +0800

    added simplifyArgs tests

--- test/logger.test.ts
+++ test/logger.test.ts
@@ -2,18 +2,40 @@
 
 
 describe("Logger", () => {
-  test("Logger instance name should be assigned during creation", async () => {
-    // some problems with open handles after tests completed https://stackoverflow.com/questions/53935108/jest-did-not-exit-one-second-after-the-test-run-has-completed-using-express
-
-    const logger = new Logger("testservice");
-    expect(logger.serviceName).toBe("testservice");
-    await logger.finish()
-    expect(1 + 2).toBe(3);
+  describe("Main functionality", () => {
+    test("Logger instance name should be assigned during creation", async () => {
+      // some problems with open handles after tests completed https://stackoverflow.com/questions/53935108/jest-did-not-exit-one-second-after-the-test-run-has-completed-using-express
+      const logger = new Logger("testservice");
+      await logger.finish();
+      expect(logger.serviceName).toBe("testservice");
+    });
   });
 
+  describe("Static functions", () => {
 
-  test("1+1", async () => {
-    expect(1 + 1).toBe(2);
+    describe("simplifyArgs", () => {
+      test("Should replace provided classes with string class name", async () => {
+        class FakeClassA {
+          constructor(public a: number, public b: string) {}
+        }
+
+        class FakeClassB {
+          constructor(public a: number, public b: string) {}
+        }
+
+        const fca = new FakeClassA(1, "a");
+        const fcb = new FakeClassB(1, "b");
+
+        const args = [1, fca, 2, fcb];
+
+        const newArgs = Logger.simplifyArgs(args, ["FakeClassA"]);
+
+
+        expect(newArgs).toStrictEqual([1, "FakeClassA", 2, fcb]);
+      });
+
+    });
+
   });
 
   afterAll(async () => {
